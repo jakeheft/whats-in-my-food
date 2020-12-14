26116575b7af7a7f7bcc191aea4e68598e6f93e3
Jake Heft <jakeheft@gmail.com>
2020-12-14 10:37:10 -0700

    Add test for results in foods index

--- spec/features/foods/index_spec.rb
+++ spec/features/foods/index_spec.rb
@@ -4,9 +4,22 @@ describe 'When I select an ingredient' do
   it "I am taken to '/foods' where I can see 10 foods that contain that ingredient" do
     visit root_path
 
-    fill_in :q, with: 'sweet potatoes'
+    search = 'sweet potatoes'
+
+    fill_in :q, with: search
     click_on 'Search'
 
     expect(current_path).to eq(foods_path)
+    expect(page).to have_content(search)
+    expect(page).to have_content('Number of results:')
+    expect(FoodFacade.result_count(search)).to_not eq(0)
+    expect(page).to have_content(FoodFacade.result_count(search))
+
+    within(first('.food')) do
+      expect(page).to have_content('Description:')
+      expect(page).to have_content('GTIN/UPC:')
+      expect(page).to have_content('Brand Owner:')
+      expect(page).to have_content('Ingredients:')
+    end
   end
 end
